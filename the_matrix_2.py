b4850ab53aa1fed915654be81c8b13a6be34944a
juan <jnfran92@gmail.com>
2020-12-26 21:34:01 -0300

    matrix!

--- the_matrix_2.py
+++ the_matrix_2.py
@@ -3,4 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+binary_image = [
+    [0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+]
+
+
+plt.imshow(binary_image)
+plt.show()
 
