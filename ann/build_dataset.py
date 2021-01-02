2e87ad33f6241b13b512137fe06d3908b423f202
juan <jnfran92@gmail.com>
2021-01-02 17:10:00 -0300

    added new scripts for ann!

---
+++ ann/build_dataset.py
@@ -0,0 +1,29 @@
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import random
+plt.ioff()
+
+image = [
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+]
+
+data_frame = pd.DataFrame(image)
+
+plt.imshow(data_frame)
+plt.show()
+
+data_folder = './ann/data/0'
+file_name = str(random.randint(0, 10**10))
+file_path = data_folder + '/' + file_name + '.csv'
+
+data_frame.to_csv(file_path, header=False, index=False)
+
---
+++ ann/main.py
@@ -0,0 +1,19 @@
+
+import numpy
+import matplotlib.pyplot as plt
+
+# 1 read data
+
+
+# 2 pre - process
+
+
+# 3 build ann model
+
+
+# 4 train model
+
+
+# 5 test model
+
+
