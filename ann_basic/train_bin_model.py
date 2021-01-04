f2d24be129bd383089e4c9a3ef470449694d1a14
nathalyusl <nathalyusl@gmail.com>
2021-01-03 21:10:12 -0500

    new name title

--- ann_basic/train_bin_model.py
+++ ann_basic/train_bin_model.py
@@ -71,8 +71,11 @@ data_set = [
     three_image_array.flatten()
 ]
 
+# Create data set
 data_set_array = np.array(data_set)
-labels = np.array([0, 1, 0, 0])
+
+
+labels = np.array([1, 0, 0, 0])
 print(data_set_array.shape)
 
 
