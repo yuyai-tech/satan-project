a682f658e922b1e63d345466902ae3210962bbb0
nathalyusl <nathalyusl@gmail.com>
2021-01-08 23:53:10 -0500

    train_alphabet_model.py

---
+++ ann_basic/train_alphabet_model.py
@@ -0,0 +1,18 @@
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+import matplotlib.pyplot as plt
+
+# load data
+
+
+
+
+
+
+
+
+
+
+
+# pre pros data
