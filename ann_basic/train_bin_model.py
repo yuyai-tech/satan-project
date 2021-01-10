04d50f2fbdc667259ccfdfe2fadff0e64a242712
juan <jnfran92@gmail.com>
2021-01-09 22:02:23 -0300

    modified train!

--- ann_basic/train_bin_model.py
+++ ann_basic/train_bin_model.py
@@ -92,6 +92,9 @@ model.add(
     Dense(50, input_dim=input_dim, activation='relu')
 )
 model.add(
+    Dense(50, input_dim=input_dim, activation='relu')
+)
+model.add(
     Dense(1, activation='sigmoid')
 )
 
