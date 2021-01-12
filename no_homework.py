b9604db941d97780205a072f71e3b37e9ba7bfe9
nathalyusl <nathalyusl@gmail.com>
2021-01-11 19:53:07 -0500

    added # reserved words

--- no_homework.py
+++ no_homework.py
@@ -5,4 +5,13 @@ print(x)
 
 x = x + 1
 print(x)
-
+# Reserved words
+words = [
+    "False  class  return   is   finally"
+    "None   if      for   lambda  continue"
+    "True   def     from   while   nonlocal"
+    "and    del    global   not     with "
+    "as     elif    try    or      yield "
+    "assert else   import  pass"
+    "break  except  in     raise "
+]
