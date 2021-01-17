b37dbf8f4ca8f7c86e1094207c772b361983eb26
juan <jnfran92@gmail.com>
2021-01-16 21:40:38 -0300

    mnist added!

--- jnfran92/mnist_lab.py
+++ jnfran92/mnist_lab.py
@@ -2,6 +2,7 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
+
 data = tf.keras.datasets.mnist.load_data()
 
 train_data = data[0][0]
@@ -11,13 +12,28 @@ print(train_data.shape)
 print(train_labels.shape)
 
 
-item_data = train_data[100]
-item_label = train_labels[100]
+item_data = train_data[530]
+item_label = train_labels[530]
 print(item_data.shape)
 
 plt.imshow(item_data)
 plt.title('LABEL: ' + str(item_label))
 plt.show()
+
+
+# creating labels
+binary_labels = []
+for value in train_labels:
+    # print(value)
+    if value == 0:
+        binary_labels.append(1)
+    else:
+        binary_labels.append(0)
+
+
+# train data
+X = train_data
+# Y =
 
 
 
