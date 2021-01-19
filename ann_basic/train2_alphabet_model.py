5d6551452db63e13f7766d493b67358ac49bce4b
nathalyusl <nathalyusl@gmail.com>
2021-01-18 21:05:40 -0500

    added data

--- ann_basic/train2_alphabet_model.py
+++ ann_basic/train2_alphabet_model.py
@@ -1,5 +1,9 @@
+import os
+os.chdir("./ann_basic/")
+
 import matplotlib as plt
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.model import Sequential
 from tensorflow.keras.layers import Dense
+from ann_basic.data.data import
