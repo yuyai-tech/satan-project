98d92ec7b786f108e01ccd7450b896ef9ae3642f
nathalyusl <nathalyusl@gmail.com>
2021-01-19 10:00:48 -0500

    Beginning model alphabet

--- ann_basic/train2_alphabet_model.py
+++ ann_basic/train2_alphabet_model.py
@@ -1,1 +1,6 @@
+from ann_basic.data.data import *
 
+## Data
+
+data_image = [ a_image, a1_image, a2_image,a3_image, b_image, c_image, d_image,
+               e_image, e1_image, e2_image, f_image, g_image,
