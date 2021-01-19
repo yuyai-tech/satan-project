fac185fdca2452f45b084b15bb93e00932d03d63
nathalyusl <nathalyusl@gmail.com>
2021-01-19 10:02:16 -0500

    ## dta

--- ann_basic/train2_alphabet_model.py
+++ ann_basic/train2_alphabet_model.py
@@ -3,4 +3,6 @@ from ann_basic.data.data import *
 ## Data
 
 data_image = [ a_image, a1_image, a2_image,a3_image, b_image, c_image, d_image,
-               e_image, e1_image, e2_image, f_image, g_image,
+               e_image, e1_image, e2_image, f_image, g_image, h_image, i_image,
+               j_image, k_image, l_image, m_image, n_image, o_image, p_image,
+               q_image,
