43dd6ec80aff108f0b826a5fae050258d5dcec30
jnfran92 <jnfran92@gmail.com>
2021-01-30 21:46:48 -0300

    adding files!

--- the-no-labs/roc_curve/mnist_more_layers_multicat_lab.py
+++ the-no-labs/roc_curve/mnist_more_layers_multicat_lab.py
@@ -8,15 +8,8 @@ from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.metrics import roc_curve
 from sklearn.metrics import roc_auc_score
 
-
-def plot_roc_curve(fpr, tpr):
-    plt.plot(fpr, tpr, color='orange', label='ROC')
-    plt.plot([0, 1], [0, 1], color='darkblue', linestyle='--')
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('Receiver Operating Characteristic (ROC) Curve')
-    plt.legend()
-    plt.show()
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
 
 
 data = tf.keras.datasets.mnist.load_data()
@@ -41,20 +34,9 @@ mlb = MultiLabelBinarizer()
 multi_cat_binary_labels = mlb.fit_transform(train_labels_reshaped)
 
 
-
-
-item_data = train_data[530]
-item_label = train_labels[530]
-print(item_data.shape)
-
-plt.imshow(item_data)
-plt.title('LABEL: ' + str(item_label))
-plt.show()
-
-
 # train data
 X = train_data
-Y = (train_labels == 5).astype(int)
+Y = multi_cat_binary_labels
 
 print(X.shape)
 print(Y.shape)
@@ -69,21 +51,39 @@ model.add(
     Flatten()
 )
 model.add(
-    Dense(5, activation='relu')
+    Dense(300, activation='relu')
 )
 model.add(
-    Dense(1, activation='sigmoid')
+    Dense(300, activation='relu')
+)
+model.add(
+    Dense(10, activation='sigmoid')
 )
 model.summary()
 
 
 # train model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(X, Y, epochs=3, batch_size=32, verbose=1)
+model.fit(X, Y, epochs=10, batch_size=256, verbose=1)
 
 
 
+# test labels
+test_labels_len = test_labels.shape[0]
+test_labels_reshaped = test_labels.reshape([test_labels_len, 1])
+
+mlb = MultiLabelBinarizer()
+multi_cat_binary_labels_test = mlb.fit_transform(test_labels_reshaped)
 
 
+X_test = test_data
+Y_test = multi_cat_binary_labels_test
+
+prediction_test = model.predict(X_test)
+prediction_test_argmax = np.argmax(prediction_test, axis=1)
+
+cm = confusion_matrix(Y_test, prediction_test_argmax)
+sns.heatmap(cm, annot=True, fmt="d", cmap="Spectral")
+plt.show()
 
 
